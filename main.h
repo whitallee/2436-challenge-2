f429988ce2a911540e5c4df91a59a83b7a25e038
Whit Allee <whitallee@gmail.com>
2026-04-22 16:04:30 -0500

    defined constants for rand_string generator

--- main.h
+++ main.h
@@ -9,10 +9,21 @@
 
 #include <iostream>
 #include <string>
+#include <random>
+#include <cstdlib>
+#include <ctime>
 #include "queue.h"
 
 using std::cout;
 using std::endl;
 using std::string;
+
+#define MAX_ID 1000
+#define CHOICES 9
+#define RANDOM_ITERATIONS 500
+#define SEED_SIZE 10
+#define RAND_STR_FIRST 'A'
+#define RAND_STR_LAST  'Z'
+#define RAND_STR_LEN   10
 
 #endif // MAIN_H
