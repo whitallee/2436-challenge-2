84474d2d56d408d33cd5870cbbb08ea09f957aca
Whit Allee <whitallee@gmail.com>
2026-04-22 14:58:07 -0500

    created boilerplate for main.cpp/h files

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/* ******************************************
+ * File:   main.cpp
+ * Author: Whit Allee
+ * Test driver for the Queue ADT.
+ * ******************************************/
+
+#include "main.h"
+
+int main() {
+
+    cout << "Queue ADT Test Driver" << endl;
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,18 @@
+/* ******************************************
+ * File:   main.h
+ * Author: Whit Allee
+ * Header for the Queue ADT test driver.
+ * ******************************************/
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <iostream>
+#include <string>
+#include "queue.h"
+
+using std::cout;
+using std::endl;
+using std::string;
+
+#endif // MAIN_H
