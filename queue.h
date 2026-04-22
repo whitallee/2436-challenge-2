0e125cba3a67d88c91d742d63978bce1be0285c1
Whit Allee <whitallee@gmail.com>
2026-04-22 14:55:34 -0500

    added Data and Node structs and Queue interface in header file

---
+++ queue.h
@@ -0,0 +1,66 @@
+/* ******************************************
+ * File:   queue.h
+ * Author: Whit Allee
+ * The interface for a Queue ADT.
+ * A single self-contained ADT that operates
+ * as either a LIFO (stack) or FIFO queue,
+ * implemented internally as a linked list.
+ * ******************************************/
+
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include <string>
+
+#define LIFO true
+#define FIFO false
+
+struct Data {
+    int id;
+    std::string information;
+};
+
+struct Node {
+    Data data;
+    Node *next;
+    Node *prev;
+};
+
+class Queue {
+
+public:
+
+    /**********************
+     * Constructors/Destructor
+     **********************/
+    Queue(bool = LIFO);
+    ~Queue();
+
+    /**********************
+     * Getters/Accessors
+     **********************/
+    int peek();
+    int count();
+    bool exists(int);
+    int find(int);
+
+    /**********************
+     * Setters/Mutators
+     **********************/
+    bool push(int, std::string&);
+    bool pull(Data&);
+    void clear();
+
+private:
+
+    /**********************
+     * Attributes
+     **********************/
+    bool type;
+    Node *head;
+    Node *tail;
+    int size;
+
+};
+
+#endif // QUEUE_H
